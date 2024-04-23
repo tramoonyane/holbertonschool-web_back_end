32b4b67635356c4410ee8088d5cfbde1d96a7eee
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-23 21:31:43 +0200

    Update 7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Create a tuple with the string k and the square of the int/float v.
