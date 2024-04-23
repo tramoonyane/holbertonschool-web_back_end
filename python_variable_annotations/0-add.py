cff97f02eb9185600b7f4381bba822139c679957
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-23 20:01:55 +0200

    Update 0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Calculate the sum of two floats.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
