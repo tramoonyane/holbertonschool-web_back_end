600cb06665fe5400c4e690faaf8c80c744ba197d
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-23 20:21:09 +0200

    Update 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Return the floor of a float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        float: The floor value of the input float.
+    """
+    return math.floor(n)
