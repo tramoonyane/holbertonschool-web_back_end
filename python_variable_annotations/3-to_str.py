aa4daa9ea7f2910389b18ec8d000da8e96acad96
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-23 20:26:03 +0200

    Update 3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
