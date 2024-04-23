bcd1219cfba0208216e830987014ad836a837fb7
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-23 20:33:57 +0200

    Update 5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculate the sum of floats in a list.
+
+    Args:
+        input_list (List[float]): The list of floats.
+
+    Returns:
+        float: The sum of floats in the list.
+    """
+    return sum(input_list)
