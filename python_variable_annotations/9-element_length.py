e609a9d16e4b412716a9d6f418776a78127e3c05
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-23 21:12:40 +0200

    Update 9-element_length.py

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """
+    Calculate the length of each element in a list of strings.
+
+    Args:
+        lst (List[str]): The list of strings.
+
+    Returns:
+        List[Tuple[str, int]]: A list of tuples where each tuple contains a string from lst and its length.
+    """
+    return [(i, len(i)) for i in lst]
