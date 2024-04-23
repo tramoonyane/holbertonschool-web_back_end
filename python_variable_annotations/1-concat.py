9c8f99ea985f84784bfe64e9078be9d693e15b13
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-23 20:15:36 +0200

    Update 1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
