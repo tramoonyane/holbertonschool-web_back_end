0ebe3705c696e21b03bc1c17fcb7b768d72a565f
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-23 21:35:47 +0200

    Update 9-element_length.py

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -11,6 +11,7 @@ def element_length(lst: List[str]) -> List[Tuple[str, int]]:
         lst (List[str]): The list of strings.
 
     Returns:
-        List[Tuple[str, int]]: A list of tuples where each tuple contains a string from lst and its length.
+        List[Tuple[str, int]]: A list of tuples where each tuple
+        contains a string from lst and its length.
     """
     return [(i, len(i)) for i in lst]
