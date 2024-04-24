23ce4a3958db1e60284a6bf958dedec251d520d6
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-24 23:51:27 +0200

    Update 2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
 """  Basic annotations -  floor   """
-import math
 
 
-def floor(n: float) -> float:
-    """
-    Return the floor of a float.
-
-    Args:
-        n (float): The input float.
-
-    Returns:
-        float: The floor value of the input float.
-    """
-    return math.floor(n)
+def floor(n: float) -> int:
+    """ function floor """
+    return int(n)
