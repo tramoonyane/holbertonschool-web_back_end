602ca3047fff8a2e2557eeed37a437a3bd349b16
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-09 05:09:46 +0200

    Update 1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    // Modification of existing variable, not redeclaration
-    task = true; // This will cause an error with const, so you may not want to use const here
-    task2 = false; // This will cause an error with const, so you may not want to use const here
+    task = true; // Modification of existing variable, not redeclaration
+    task2 = false; // Modification of existing variable, not redeclaration
   }
 
   return [task, task2];
